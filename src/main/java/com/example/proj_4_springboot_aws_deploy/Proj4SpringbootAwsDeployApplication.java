39b04e9b9126b4e2560ef68d46006b6937fa4074
pratiksontakke <pratikass488@gmail.com>
2025-03-17 13:26:17 +0530

    initial commit

---
+++ src/main/java/com/example/proj_4_springboot_aws_deploy/Proj4SpringbootAwsDeployApplication.java
@@ -0,0 +1,13 @@
+package com.example.proj_4_springboot_aws_deploy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Proj4SpringbootAwsDeployApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Proj4SpringbootAwsDeployApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/proj_4_springboot_aws_deploy/Proj4SpringbootAwsDeployApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.proj_4_springboot_aws_deploy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Proj4SpringbootAwsDeployApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
